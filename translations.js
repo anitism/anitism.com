ba5fceb496d36b313841463a3bb85bf876bd2b25
Francis Brandon van Balkom <79877241+anitism@users.noreply.github.com>
2026-06-23 00:21:35 +0200

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,6 @@
+while(translated.includes("Hello")){
+  translated = translated.replace("Hello", "Hey ");
+}
+while(translated.includes("Test")){
+  translated = translated.replace("Test", "Test1 ");
+}
